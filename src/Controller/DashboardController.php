51d36f65a60a4076587dbb0cfdff0497c5f5e394
Djyp <d@tbnk.ca>
2020-11-08 12:11:31 +0100

    Ajout de la connexion

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -16,7 +16,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardController extends AbstractDashboardController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/", name="admin")
      */
     public function index(): Response
     {
